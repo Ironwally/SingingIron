0600d05f42841d242ae5e1f5026528b32d03fb5d
Eisern <notmyname.eisern@gmail.com>
2024-02-04 03:06:08 +0100

    Cog post-reloading works. rest can wait. next step: Voice channel features

--- cogs/PostProcessing.py
+++ cogs/PostProcessing.py
@@ -8,38 +8,42 @@ class PostProcessing(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    cogs = app_commands.Group(name="cogs", description="Commands for loading, reloading and unloading cogs")
+    #cogs = app_commands.Group(name="cogs", description="Commands for loading, reloading and unloading cogs")
+
+    @commands.group()
+    async def cogs(self, ctx: commands.Context):
+        if ctx.invoked_subcommand is None:
+            await ctx.send("Group command not found")
 
     @cogs.command(description='Reload a cog')
-    async def reload(self, interaction: discord.Interaction, cog: str):
+    async def reload(self, ctx: commands.Context, cog: str = None):
+        # Should be safe from code injection because of one-word-only argument. If not, lower code should fix.
+        # if ctx.bot.get_cog(cog) is None: raise commands.BadArgument(
+        #    "Error. This Cog does not exist or is not loaded.")
         try:
-            if not cog in [cogg[:-3] for cogg in os.listdir('./cogs')]: raise commands.BadArgument(
-                "Error. Cog name does not exist.")
             await self.bot.reload_extension(f'cogs.{cog}')
-            await interaction.response.send_message(f"Reloaded cog: {cog}")
-        except commands.ExtensionNotLoaded:
-            await interaction.response.send_message(f"Cog: {cog} could not be reloaded")
+            await ctx.send(f"Reloaded cog: {cog}")
         except commands.ExtensionNotFound:
-            await interaction.response.send_message(f"Cog: {cog} not found")
+            await ctx.send(f"Cog: {cog} not found")
         except commands.BadArgument:
-            await interaction.response.send_message("Error. Cog name does not exist.")
+            await ctx.send("Error. Cog name does not exist.")
         except Exception as e:
-            await interaction.response.send_message(f"An error occurred while reloading cog {cog}: {e}")
+            await ctx.send(f"An Exception has occured: {e}")
 
     @cogs.command(description='List loaded cogs')
-    async def list(self, interaction: discord.Interaction):
+    async def list(self, ctx: commands.Context):
         extensions = (str(ex) for ex in self.bot.cogs.keys())
-        await interaction.response.send_message(f"Loaded extensions:\t{", \t".join(extensions)}")
+        await ctx.send(f"Loaded extensions:\t{", \t".join(extensions)}")
 
     @cogs.command(description='load new extensions')
-    async def load(self, interaction: discord.Interaction, cog: str = None):
+    async def load(self, ctx: commands.Context, cog: str = None):
         if cog:
             # load specified extensions
             pass
         else:
             # load all new extensions
             pass
-        await interaction.response.send_message("extension post-loading not implemented jet.")
+        await ctx.send("extension post-loading not implemented jet.")
 
 
 async def setup(bot):
